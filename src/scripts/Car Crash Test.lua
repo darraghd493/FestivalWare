5cfe7f4882226b17d7740f87c8bdf5bd412c383d
darraghd493 <darraghdowsett493@gmail.com>
2022-09-12 01:49:49 +0100

    Quickly push the Car Crash Test destroy script

--- src/scripts/Car Crash Test.lua
+++ src/scripts/Car Crash Test.lua
@@ -1,1 +1,12 @@
-local UI = loadstring(game:HttpGet(""))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/darraghd493/FestivalWare/master/src/ui/UI.lua"))()
+local Window = UI:CreateWindow("Car Crash Test")
+local Crasher = Window:CreateTab("Crasher")
+
+local Workspace = game:GetService("Workspace")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+Crasher:CreateButton("Destroy Everything", function()
+    for i, v in pairs(Workspace:GetChildren()) do
+        ReplicatedStorage.DeleteCar:FireServer(v)
+    end
+end)
